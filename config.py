189463c5d52c9573a1910f96faacb8e870372c3f
naqeebahmed156947-bit <naqeebahmed156947@gmail.com>
2026-07-13 17:26:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+import os
 
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHANNEL = os.getenv("TELEGRAM_CHANNEL")
+SERPAPI_KEY = os.getenv("SERPAPI_KEY")
+
+if not TELEGRAM_TOKEN:
+    raise Exception("TELEGRAM_TOKEN is missing")
+
+if not TELEGRAM_CHANNEL:
+    raise Exception("TELEGRAM_CHANNEL is missing")
+
+if not SERPAPI_KEY:
+    raise Exception("SERPAPI_KEY is missing")
