ecbe43722c19d0b61fb7be85c93a30cdd4b85ae7
naqeebahmed156947-bit <naqeebahmed156947@gmail.com>
2026-07-13 17:27:53 +0300

    Implement Amazon search function using SerpApi

--- amazon_api.py
+++ amazon_api.py
@@ -1,1 +1,20 @@
+import requests
+from config import SERPAPI_KEY
 
+BASE_URL = "https://serpapi.com/search"
+
+
+def search_amazon(query):
+    params = {
+        "engine": "amazon",
+        "amazon_domain": "amazon.eg",
+        "api_key": SERPAPI_KEY,
+        "k": query
+    }
+
+    response = requests.get(BASE_URL, params=params)
+
+    if response.status_code != 200:
+        raise Exception(f"SerpApi Error: {response.text}")
+
+    return response.json()
